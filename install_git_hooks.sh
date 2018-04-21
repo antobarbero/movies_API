105baea87d3128c39a420b70390af7fa8122e7a9
anto <antobarbero94@gmail.com>
2018-04-21 10:46:50 -0300

    update in hooks command

--- install_git_hooks.sh
+++ install_git_hooks.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Adds pre-commit hook into git hooks directory
-ln -b -f tools/hooks/pre-commit .git/hooks/pre-commit
+ln -b -f hooks/pre-commit .git/hooks/pre-commit
 if [ $? = 0 ]; then
     printf "The hooks have been installed correctly.\n";
 fi
