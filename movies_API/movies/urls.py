0178c3ff88f36727ee619eb9f9cdccdb44c4a411
anto <antobarbero94@gmail.com>
2018-04-22 20:00:51 -0300

    urls

--- movies_API/movies/urls.py
+++ movies_API/movies/urls.py
@@ -1,17 +1,39 @@
 from django.conf.urls import include, url
 
-from .views import PersonListAPIView, MovieListAPIView
-
+from .views import (MovieCreateAPIView, MovieRetrieveAPIView,
+                    MoviesListAPIView, MovieUpdateAPIView, PeopleListAPIView,
+                    PersonCreateAPIView, PersonRetrieveAPIView,
+                    PersonUpdateAPIView)
 
 app_name = 'movies'
+
 
 urlpatterns = [
     # people
     url(r'^people/', include([
         url(
             r'^$',
-            PersonListAPIView.as_view(),
+            PeopleListAPIView.as_view(),
             name='people-list'
+        ),
+        url(
+            r'^create/$',
+            PersonCreateAPIView.as_view(),
+            name='person-create'
+        ),
+        url(
+            r'^(?P<pk>\d+)/', include([
+                url(
+                    r'^$',
+                    PersonRetrieveAPIView.as_view(),
+                    name='person-detail'
+                ),
+                url(
+                    r'^update/$',
+                    PersonUpdateAPIView.as_view(),
+                    name='person-update'
+                ),
+            ])
         ),
     ])),
 
@@ -19,8 +41,27 @@ urlpatterns = [
     url(r'^movies/', include([
         url(
             r'^$',
-            MovieListAPIView.as_view(),
-            name='movie-list'
+            MoviesListAPIView.as_view(),
+            name='movies-list'
+        ),
+        url(
+            r'^create/$',
+            MovieCreateAPIView.as_view(),
+            name='movie-create'
+        ),
+        url(
+            r'^(?P<pk>\d+)/', include([
+                url(
+                    r'^$',
+                    MovieRetrieveAPIView.as_view(),
+                    name='movie-detail'
+                ),
+                url(
+                    r'^update/$',
+                    MovieUpdateAPIView.as_view(),
+                    name='movie-update'
+                ),
+            ])
         ),
     ])),
 ]
